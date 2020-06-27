9ad96fb87536dbbbe714d02154ff9f26e78e3f6e
Marcvs101 <marcocuoci@yahoo.it>
2020-06-27 20:24:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from mpl_toolkits import mplot3d
 from sklearn.cluster import AffinityPropagation
 
 from random import randint
 
 import numpy as np
+from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
@@ -24,21 +24,45 @@ datamatrix = list()
 
 for line in lines:
     data = line.split(":")
-    x.append(float(data[2]))
-    y.append(float(data[3]))
-    z.append(float(data[4]))
     datamatrix.append([float(data[2]),float(data[3]),float(data[4])])
 
 # Run clustering algorithm
-clustering = AffinityPropagation().fit(datamatrix)
-print(len(clustering.cluster_centers_))
-print(clustering.labels_)
+af = AffinityPropagation().fit(datamatrix)
 
-clustercolorlist = list()
-clustercolors = cm.rainbow(np.linspace(0, 1, len(clustering.cluster_centers_)))
-for item in clustering.labels_:
-    clustercolorlist.append(clustercolors[item])
+cluster_centers_indices = af.cluster_centers_indices_
+labels = af.labels_
+n_clusters_ = len(cluster_centers_indices)
 
+print('Estimated number of clusters: %d' % n_clusters_)
+print("Estimated GPS coordinates of centroids:")
+counter = 0
+for center in af.cluster_centers_:
+    print("GPS:Cluster "+str(counter)+":"+str(center[0])+":"+str(center[1])+":"+str(center[2])+":")
+    counter = counter+1
+
+clusterlist = list()
+for i in range(n_clusters_):
+    clusterlist.append([list(),list(),list()])
+clustercolors = cm.rainbow(np.linspace(0, 1, n_clusters_))
+
+counter = 0
+for item in labels:
+    clusterlist[item][0].append(datamatrix[counter][0])
+    clusterlist[item][1].append(datamatrix[counter][1])
+    clusterlist[item][2].append(datamatrix[counter][2])
+    counter = counter + 1
+    
 # Draw plot
-ax.scatter(x,y,z,c=clustercolorlist,marker='.',alpha=0.6)
+for cluster in range(n_clusters_):
+    ax.scatter(clusterlist[cluster][0],clusterlist[cluster][1],clusterlist[cluster][2],
+               color=clustercolors[cluster],marker='.',alpha=0.3,label=str(cluster))
+
+counter = 0
+for center in af.cluster_centers_:
+    ax.scatter(center[0],center[1],center[2],
+               color=clustercolors[counter],marker='v',alpha=1)
+    counter = counter+1
+
+ax.legend()
+plt.title('Estimated number of clusters: %d' % n_clusters_)
 plt.show()
